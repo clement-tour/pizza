2110c88ed021ebe6a895df83e205643d6f94857f
clement-tour <86768151+clement-tour@users.noreply.github.com>
2021-09-24 21:16:33 +0400

    Add pizza du chef

--- sources/pizza.py
+++ sources/pizza.py
@@ -35,5 +35,9 @@ RECIPES = {
     'mediterranea': {
         'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3},
         'price': 11,
+    },
+    'La pizza du chef': {
+        'ingredients': {'mozzarella': 3, 'pepper': 4, 'onion': 3, 'ham': 4, 'mushroom': 2},
+        'price': 14,
     }
 }
